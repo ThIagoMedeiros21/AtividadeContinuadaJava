9caf587328e92e0eb1a0a6aad7fc94fac3008377
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2025-09-15 19:56:56 -0300

    Creation classes Cliente and Contato

---
+++ AtividadeContinuadaPOO/src/br/edu/cs/poo/ac/ordem/entidades/Cliente.java
@@ -0,0 +1,40 @@
+package br.edu.cs.poo.ac.ordem.entidades;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Cliente {
+    private String cpfCnpj;
+    private String nome;
+    private Contato contato;
+    private LocalDate dataCadastro;
+
+    public Cliente(String cpfCnpj, String nome, Contato contato, LocalDate dataCadastro){
+        this.cpfCnpj = cpfCnpj;
+        this.nome = nome;
+        this.contato = contato;
+        this.dataCadastro = dataCadastro;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Contato getContato() {
+        return contato;
+    }
+    public void setContato(Contato contato) {
+        this.contato = contato;
+    }
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+    public int getIdadeCadastro() {
+        LocalDate hoje = LocalDate.now();
+        return Period.between(this.dataCadastro, hoje).getYears();
+    }
+}

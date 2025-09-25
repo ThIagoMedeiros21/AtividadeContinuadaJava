8717d6618c32c075097eaa0f8ac36caebbd161a5
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2025-09-25 09:41:15 -0300

    remove Dao in wrong local

--- AtividadeContinuadaPOO/src/br/edu/cs/poo/ac/ordem/daos/exemplo/NotebookDAO.java
+++
@@ -1,6 +0,0 @@
-package br.edu.cs.poo.ac.ordem.daos.exemplo;
-//O identificador �nico, por objeto, de Notebook � a concatena��o do retorno 
-//do m�todo getTipo com o atributo serial.   
-public class NotebookDAO {
-
-}
